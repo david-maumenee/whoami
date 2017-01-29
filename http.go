ca69935e1864c0141f8116eb8a4f15c47a593b55
David MAUMENEE <d.maumenee@groupeonepoint.com>
2017-01-29 19:06:40 +0100

    call a backend

--- http.go
+++ http.go
@@ -13,11 +13,22 @@ func main() {
         port = "8080"
     }
 
+    backEndURL = os.Getenv("BACK_END_URL")
+    if backEndURL == "" {
+        backEndURL = "http://backend"
+    }
+
+
     fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
+
+
     hostname, _ := os.Hostname()
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(os.Stdout, "I'm %s\n", hostname)
- 	fmt.Fprintf(w, "I'm %s\n", hostname)
+
+        resp, err := http.Get(backEndURL)
+
+        fmt.Fprintf(os.Stdout, "I'm %s - backend response : %s \n", hostname, resp)
+ 	    fmt.Fprintf(w, "I'm %s - backend response : %s \n", hostname, resp)
     })
 
 
